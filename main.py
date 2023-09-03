1fd6dddf9dcf2d4e24365ba7bd85084f9e2d3028
skop-22 <skop@gmail.com>
2023-09-02 20:21:40 -0600

    configurando main para la vista de qml

---
+++ main.py
@@ -0,0 +1,14 @@
+#librerias
+
+
+#clase principal
+class windowPrin():
+    def __init__(self,parent=None):
+        pass
+    pass
+
+
+#objeto
+if __name__ == "__main__":
+    window = windowPrin()
+    pass
