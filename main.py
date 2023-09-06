c366e0a1c68371f59c4eb26d2cda5e5eed9e3d7a
skop-22 <skop@gmail.com>
2023-09-06 06:51:04 -0600

    Cambios a ser confirmados:
    configurando archivos inecesarios
    	nuevos archivos: .gitignore
    
    configurando archivos para la interface grafica
    	nuevos archivos: GUI/Interface/VentanaPrincipal.ui
    	nuevos archivos: GUI/ui_VentanaPrincipal.py
    
    configuración para mostrar la ventana
    	modificados:     main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-#librerias
+# archibos necesaris
+from GUI.ui_VentanaPrincipal import *
+# librerias
+import sys
 
 
-#clase principal
-class windowPrin():
-    def __init__(self,parent=None):
-        pass
+# clase principal
+class windowPrin(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
+        self.anchoBanda = Ui_MainWindow()
     pass
 
 
-#objeto
+# objeto
 if __name__ == "__main__":
+    app = QApplication(sys.argv)
     window = windowPrin()
+    window.show()
+    sys.exit(app.exec())
     pass
