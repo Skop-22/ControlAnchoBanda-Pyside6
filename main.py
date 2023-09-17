bb67d6c49de2d1e60d672c60e9656bc749764ddf
Skop-22 <skopflores@gmail.com>
2023-09-17 13:01:31 -0600

    Configuración de la vista principal En la rama QFluenteWindow Cambios a ser confirmados: 	modificados:     GUI/Interface/VentanaPrin2.ui 	modificados:     GUI/ui_VentanaPrin2.py 	modificados:     main.py

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 # archibos necesaris
-from GUI.ui_VentanaPrin2 import Ui_Form
-from PySide6.QtWidgets import QApplication
-from PySide6.QtCore import Qt
-from qframelesswindow import FramelessWindow
-from qfluentwidgets import *
+from GUI.ui_VentanaPrin2 import *
+from qframelesswindow import AcrylicWindow
+from qfluentwidgets import setThemeColor
+from qfluentwidgets import FluentTranslator, SplitTitleBar
 # librerias
 import sys
 
 # clase principal
-class windowPrin(FramelessWindow, Ui_Form):
+class windowPrin(AcrylicWindow,Ui_Form):
 
-    def __init__(self,parent=None):
-        super().__init__(parent)
+    def __init__(self):
+        super().__init__()
         self.setupUi(self)
+        # change theme color
+        setThemeColor('#28afe9')
+
+        # change title bar
+        self.setTitleBar(SplitTitleBar(self))
+        self.titleBar.raise_()
+
+        self.setWindowTitle('Control De Ancho De Banda')
+        self.windowEffect.setMicaEffect(self.winId())
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    #app.installTranslator(FluentTranslator(QLocale()))
     window = windowPrin()
     window.show()
     sys.exit(app.exec())
