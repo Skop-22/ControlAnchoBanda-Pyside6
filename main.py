5e23de66dd6879f390105d7a360fac1fefa3c14f
Skop-22 <skopflores@gmail.com>
2023-09-17 11:00:33 -0600

    Modificacion de los archivos principales pra la ejecución de QFluenteWindow
    En la rama QFluenteWindow
    
      Cambios a ser confirmados:
    	modificados:     GUI/Interface/VentanaPrin2.ui
    	borrados:        GUI/Interface/VentanaPrincipal.ui
    	modificados:     GUI/ui_VentanaPrin2.py
    	borrados:        GUI/ui_VentanaPrincipal.py
    	modificados:     main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 # archibos necesaris
 from GUI.ui_VentanaPrin2 import Ui_Form
-from PySide6.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout
+from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import Qt
-from qfluentwidgets import MSFluentWindow
+from qframelesswindow import FramelessWindow
+from qfluentwidgets import *
 # librerias
 import sys
 
-
 # clase principal
-class windowPrin(MSFluentWindow, Ui_Form):
-    def __init__(self):
-        super().__init__()
+class windowPrin(FramelessWindow, Ui_Form):
+
+    def __init__(self,parent=None):
+        super().__init__(parent)
         self.setupUi(self)
 
 if __name__ == "__main__":
-
     app = QApplication(sys.argv)
     window = windowPrin()
     window.show()
